fc407d73dee91ed8dfcdde27f09e40ca3a117675
Zerensky <pure.evil97@mail.ru>
2023-07-14 19:16:21 +0600

    init file

---
+++ _init_.py
@@ -0,0 +1,8 @@
+from access_level_error import *
+from User_class import *
+from user_exceptions import *
+from student import *
+from student import *
+from user_exceptions import UserTypeStrError, UserTypeTextError, UserLessonsError, UserEstimateError
+
+__all__ = ['User_class', 'access_level_error', 'student', 'exception_numeric_data', 'exception_dict', 'user_exceptions']
